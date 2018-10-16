a7e530fb27bc7daff3b06972f47396c528d16a28
Valeriia Fatova <valeriiafatova@Air-Valeriia.kyiv.epam.com>
2018-10-16 17:58:55 +0300

    init web app with java config

---
+++ src/main/java/com/epam/rest/configuration/RestAppSpringConfiguration.java
@@ -0,0 +1,28 @@
+package com.epam.rest.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.epam.rest")
+public class RestAppSpringConfiguration extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public ViewResolver viewResolver()
+    {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+
+        return viewResolver;
+    }
+}
